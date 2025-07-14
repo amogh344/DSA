4f40ab8c2b535c65331a6331c7860dbeba9ed529
Amogh Brahma R <abr@Amoghs-MacBook-Air.local>
2025-07-14 10:50:11 +0530

    Few changes made to ArraysCC.java

--- Arrays/ArraysCC.java
+++ Arrays/ArraysCC.java
@@ -1,4 +1,24 @@
 import java.util.*;
 
 public class ArraysCC {
+
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("Phy : "+marks[0]);
+        System.out.println("Math : "+marks[1]);
+        System.out.println("Chem : "+marks[2]);
+
+        int percentage = ((marks[0]+marks[1]+marks[2])/300)*100;
+
+        System.out.println("Percentage : "+percentage);
+        sc.close();
+    }
+    
 }
