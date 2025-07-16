3c57d3dd084fb27eb1c83792bfa752c705c233a9
amogh344 <amoghbrahma@gmail.com>
2025-07-17 00:08:23 +0530

    modified

--- Arrays/TrappingRainwater.java
+++ Arrays/TrappingRainwater.java
@@ -3,28 +3,38 @@
 
 public class TrappingRainwater {
     public static int trappedRainwater(int height[]) {
-        int n=height.length;
+
         // calculate left max boundary
+        int n = height.length;
         int leftMax[] = new int[n];
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
+
         // calculate right max boundary
-        int rightmax[] = new int[n];
-        rightmax[n-1]=height[n]
-        for (int i = n-1); i >1; i--) {
-            rightmax[i]=Math.max(rightmax[i+1], height[i])
-            
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
+        int trappedWater = 0;
+
         // loop
-        // waterLevel = min(leftmax,rightmax)
-        // trapped water = waterLevel - height[i]
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+        // waterlevel=min(leftmax,rightmax)
+        // trapped water = waterlevel - height[i]
 
     }
 
     public static void main(String[] args) {
         int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println("Volume of Trapped Rainwater : " + trappedRainwater(height));
     }
 
 }
