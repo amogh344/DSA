244c75b4f2d807b644a6c8b33ed547c055032c12
Amogh Brahma R <abr@Amoghs-MacBook-Air.local>
2025-07-19 20:31:58 +0530

    added bubble sort

--- Basic_Sorting_Algorithms/BubbleSort.java
+++ Basic_Sorting_Algorithms/BubbleSort.java
@@ -1,5 +1,31 @@
 package Basic_Sorting_Algorithms;
 
+import java.util.*;
+
 public class BubbleSort {
-    
+
+    public static int[] bubbleSort(int arr[])
+    {
+        for (int turn = 0; turn < arr.length; turn++) {
+            for (int j = 0; j < arr.length-1-turn; j++) {
+                if (arr[j]>arr[j+1]) {
+                    
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1] = temp;
+                    
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        int sort[] = bubbleSort(arr);
+
+        for (int i : sort) {
+            System.out.print(i + " "); 
+        }
+    }
 }
